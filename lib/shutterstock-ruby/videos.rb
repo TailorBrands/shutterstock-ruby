8b172d09fe94eaf5e1389c29215a9bef53e7f1a7
Bruno Joseph <Bruno.Joseph95@gmail.com>
2016-08-12 11:23:36 +0200

    fix encoding issue

--- lib/shutterstock-ruby/videos.rb
+++ lib/shutterstock-ruby/videos.rb
@@ -4,7 +4,6 @@ module ShutterstockRuby
     extend Connections
 
     def self.search(query, options = {})
-      query = URI.encode_www_form_component(query)
       JSON.parse(self.get('/videos/search', { query: query }.merge(options)))
     end
   end
