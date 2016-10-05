30c6dc8eb73ac482521347c96d237b6395fab820
William Bowling <will@biteable.com>
2016-10-05 12:51:27 +1100

    allow access_token for authentication

--- spec/lib/shutterstock_ruby_spec.rb
+++ spec/lib/shutterstock_ruby_spec.rb
@@ -2,26 +2,32 @@ require 'spec_helper'
 
 RSpec.describe ShutterstockRuby do
   after :each do
+    ShutterstockRuby.configuration.access_token = nil
     ShutterstockRuby.configuration.api_client = nil
     ShutterstockRuby.configuration.api_secret = nil
   end
 
   it 'has the correct default' do
+    expect(ShutterstockRuby.configuration.access_token).to be nil
     expect(ShutterstockRuby.configuration.api_client).to be nil
     expect(ShutterstockRuby.configuration.api_secret).to be nil
   end
 
   it 'sets the correct configuration' do
+    expect(ShutterstockRuby.configuration.access_token).to be nil
     expect(ShutterstockRuby.configuration.api_client).to be nil
     expect(ShutterstockRuby.configuration.api_secret).to be nil
 
+    token = SecureRandom.uuid
     key = SecureRandom.uuid
     secret = SecureRandom.uuid
     ShutterstockRuby.configure do |config|
+      config.access_token = token
       config.api_client = key
       config.api_secret = secret
     end
 
+    expect(ShutterstockRuby.configuration.access_token).to equal(token)
     expect(ShutterstockRuby.configuration.api_client).to equal(key)
     expect(ShutterstockRuby.configuration.api_secret).to equal(secret)
   end
