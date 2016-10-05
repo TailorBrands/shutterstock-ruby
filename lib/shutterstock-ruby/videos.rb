77eb0c00026c40fc19aeade5e4f58269b8cecd8b
William Bowling <will@biteable.com>
2016-10-05 12:51:56 +1100

    add purchase for videos

--- lib/shutterstock-ruby/videos.rb
+++ lib/shutterstock-ruby/videos.rb
@@ -6,5 +6,11 @@ module ShutterstockRuby
     def self.search(query, options = {})
       JSON.parse(self.get('/videos/search', { query: query }.merge(options)))
     end
+
+    def self.purchase(id, subscription_id, size, options = {})
+      params = { subscription_id: subscription_id, size: size }
+      body = { videos: [ video_id: id ] }.to_json
+      JSON.parse(self.post("/videos/licenses", body, params, options))
+    end
   end
 end
